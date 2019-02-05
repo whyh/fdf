28c2a07ad1c4e65266e65c20738980e526fc1896
dderevyn <danderevinsky@gmail.com>
2019-02-05 13:46:46 +0200

    changed cmake

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -50,5 +50,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/ffdf.dir/DependInfo.cmake"
+  "CMakeFiles/fdf_clion.dir/DependInfo.cmake"
   )
